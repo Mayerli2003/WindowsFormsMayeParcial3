ea33f44ed1527d2d07836c95f3ad6072b0534275
Mayerli2003 <87994175+Mayerli2003@users.noreply.github.com>
2022-06-20 21:40:22 -0600

    WFormsMayerli

---
+++ WFormsMayerli/WFormsMayerli/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace WFormsMayerli
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textNombre = new System.Windows.Forms.TextBox();
+            this.Agregar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Descripción = new System.Windows.Forms.Label();
+            this.textDescripcion = new System.Windows.Forms.TextBox();
+            this.textPrecio = new System.Windows.Forms.TextBox();
+            this.textCantidad = new System.Windows.Forms.TextBox();
+            this.textCodigo = new System.Windows.Forms.TextBox();
+            this.textProveedor = new System.Windows.Forms.TextBox();
+            this.textEstado = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Cerrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textNombre
+            // 
+            this.textNombre.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textNombre.Location = new System.Drawing.Point(159, 32);
+            this.textNombre.Name = "textNombre";
+            this.textNombre.Size = new System.Drawing.Size(249, 29);
+            this.textNombre.TabIndex = 1;
+            // 
+            // Agregar
+            // 
+            this.Agregar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.Agregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Agregar.Location = new System.Drawing.Point(574, 406);
+            this.Agregar.Name = "Agregar";
+            this.Agregar.Size = new System.Drawing.Size(79, 32);
+            this.Agregar.TabIndex = 2;
+            this.Agregar.Text = "Agregar";
+            this.Agregar.UseVisualStyleBackColor = false;
+            this.Agregar.Click += new System.EventHandler(this.Agregar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(49, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Precio:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(49, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 21);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Codigo:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(49, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 21);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Cantidad:";
+            // 
+            // Descripción
+            // 
+            this.Descripción.AutoSize = true;
+            this.Descripción.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Descripción.Location = new System.Drawing.Point(49, 67);
+            this.Descripción.Name = "Descripción";
+            this.Descripción.Size = new System.Drawing.Size(102, 21);
+            this.Descripción.TabIndex = 6;
+            this.Descripción.Text = "Descripción:";
+            // 
+            // textDescripcion
+            // 
+            this.textDescripcion.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textDescripcion.Location = new System.Drawing.Point(159, 67);
+            this.textDescripcion.Name = "textDescripcion";
+            this.textDescripcion.Size = new System.Drawing.Size(249, 29);
+            this.textDescripcion.TabIndex = 7;
+            // 
+            // textPrecio
+            // 
+            this.textPrecio.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textPrecio.Location = new System.Drawing.Point(159, 107);
+            this.textPrecio.Name = "textPrecio";
+            this.textPrecio.Size = new System.Drawing.Size(249, 29);
+            this.textPrecio.TabIndex = 8;
+            // 
+            // textCantidad
+            // 
+            this.textCantidad.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textCantidad.Location = new System.Drawing.Point(159, 145);
+            this.textCantidad.Name = "textCantidad";
+            this.textCantidad.Size = new System.Drawing.Size(249, 29);
+            this.textCantidad.TabIndex = 9;
+            // 
+            // textCodigo
+            // 
+            this.textCodigo.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textCodigo.Location = new System.Drawing.Point(159, 184);
+            this.textCodigo.Name = "textCodigo";
+            this.textCodigo.Size = new System.Drawing.Size(249, 29);
+            this.textCodigo.TabIndex = 10;
+            // 
+            // textProveedor
+            // 
+            this.textProveedor.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textProveedor.Location = new System.Drawing.Point(159, 221);
+            this.textProveedor.Name = "textProveedor";
+            this.textProveedor.Size = new System.Drawing.Size(249, 29);
+            this.textProveedor.TabIndex = 11;
+            // 
+            // textEstado
+            // 
+            this.textEstado.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textEstado.Location = new System.Drawing.Point(159, 258);
+            this.textEstado.Name = "textEstado";
+            this.textEstado.Size = new System.Drawing.Size(249, 29);
+            this.textEstado.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(49, 224);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 21);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Proveedor:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(49, 266);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 21);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Estado:";
+            // 
+            // Cerrar
+            // 
+            this.Cerrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.Cerrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cerrar.Location = new System.Drawing.Point(688, 406);
+            this.Cerrar.Name = "Cerrar";
+            this.Cerrar.Size = new System.Drawing.Size(79, 32);
+            this.Cerrar.TabIndex = 15;
+            this.Cerrar.Text = "Cerrar";
+            this.Cerrar.UseVisualStyleBackColor = false;
+            this.Cerrar.Click += new System.EventHandler(this.Cerrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Cerrar);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textEstado);
+            this.Controls.Add(this.textProveedor);
+            this.Controls.Add(this.textCodigo);
+            this.Controls.Add(this.textCantidad);
+            this.Controls.Add(this.textPrecio);
+            this.Controls.Add(this.textDescripcion);
+            this.Controls.Add(this.Descripción);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Agregar);
+            this.Controls.Add(this.textNombre);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textNombre;
+        private System.Windows.Forms.Button Agregar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label Descripción;
+        private System.Windows.Forms.TextBox textDescripcion;
+        private System.Windows.Forms.TextBox textPrecio;
+        private System.Windows.Forms.TextBox textCantidad;
+        private System.Windows.Forms.TextBox textCodigo;
+        private System.Windows.Forms.TextBox textProveedor;
+        private System.Windows.Forms.TextBox textEstado;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button Cerrar;
+    }
+}
+
---
+++ WFormsMayerli/WFormsMayerli/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WFormsMayerli
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        //Crear un registro
+        private void Agregar_Click(object sender, EventArgs e)
+        {
+            using (SuperMercadoEntities dato = new SuperMercadoEntities())
+            {
+                Articulo oArticulo = new Articulo();
+                oArticulo.nombre = textNombre.Text;
+                oArticulo.descripcion = textDescripcion.Text;
+                oArticulo.precio = decimal.Parse(textPrecio.Text);
+                oArticulo.cantidad = int.Parse(textCantidad.Text);
+                oArticulo.codigo = textCodigo.Text;
+                oArticulo.proveedor = textProveedor.Text;
+                oArticulo.estado = int.Parse(textEstado.Text);
+
+                dato.Articulo.Add(oArticulo);
+                dato.SaveChanges();
+                MessageBox.Show("Se ha creado correctamente");
+            }
+        }
+
+        private void Cerrar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
